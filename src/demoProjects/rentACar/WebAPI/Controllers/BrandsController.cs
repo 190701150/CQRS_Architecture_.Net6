401e0e7608931650c068e35b3174d34866a3fc53
ahmethamzaustun <a.hamzaa@icloud.com>
2022-09-03 21:47:45 +0300

    Add GetByIdQuery

--- src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
+++ src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Application.Features.Brands.Commands.CreateBrand;
 using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Models;
+using Application.Features.Brands.Queries.GetByIdBrand;
+using Application.Features.Brands.Queries.GetListBrand;
+using Core.Application.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,5 +23,21 @@ namespace WebAPI.Controllers
             CreatedBrandDto result = await Mediator.Send(createBrandCommand);
             return Created("", result);
         }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListBrandQuery getListBrandQuery = new() { PageRequest = pageRequest };
+            BrandListModel result = await Mediator.Send(getListBrandQuery);
+            return Ok(result);
+        }
+        
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdBrandQuery getByIdBrandQuery)
+        {
+            BrandGetByIdDto brandGetByIdDto = await Mediator.Send(getByIdBrandQuery);
+            return Ok(brandGetByIdDto);
+        }
+        
     }
 }
